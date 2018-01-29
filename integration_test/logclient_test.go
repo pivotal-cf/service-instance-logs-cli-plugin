302f928d8dad63f094615e78733d0e87a015fb99
Mike Heath <mheath@pivotal.io>
2018-01-29 14:28:30 -0700

    Fix integration test to use loopback network interface

--- integration_test/logclient_test.go
+++ integration_test/logclient_test.go
@@ -21,7 +21,7 @@ import (
 
 const (
 	testServerPort              = "8888"
-	testServerAddress           = "0.0.0.0:" + testServerPort
+	testServerAddress           = "127.0.0.1:" + testServerPort
 	endpointUrl                 = "ws://" + testServerAddress
 	requestedNumberOfLogEntries = 10
 	logTimestampFormat          = "2006-01-02T15:04:05.00-0700"
